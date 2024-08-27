346a62821e48192136a5d0c5fff83c6af7d711db
ashishverma2831 <ashishverma510207@gmail.com>
2024-08-27 15:55:03 +0530

    initial setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='bg-red-500'>Hello World</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
